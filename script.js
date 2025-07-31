7cf604b0217d470cf267fbe7f043ca1937b3d7ef
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2025-07-31 12:42:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let selectedValue;
+document.getElementById("colorSelect").addEventListener("change", function () {
+    const elements = document.getElementById("colorSelect").children;
+    selectedValue = this.value;
+    
+})
+
+document.querySelector("input[type='button']").addEventListener("click", function () {
+    const elements = document.getElementById("colorSelect").children;
+
+    for(let i = 0; i < elements.length; i++) {
+        if(elements[i].textContent === selectedValue) {
+            elements[i].remove();
+        }
+    }
+})
